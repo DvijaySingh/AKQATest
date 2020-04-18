2cfd5b1e1c921420391333dde8caf8f52b7d32e5
durgv <durgvijayrajput@gmail.com>
2020-04-18 19:02:42 +0530

    Add API controller

--- AKQA.Models/Employee.cs
+++ AKQA.Models/Employee.cs
@@ -1,8 +1,13 @@
-﻿namespace AKQA.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AKQA.Models
 {
     public class Employee
     {
+        [Required(ErrorMessage ="Please Enter Name") ]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Number")]
+        [Display(Name ="Number")]
         public decimal Salary { get; set; }
         public string SalaryString { get; set; }
     }
